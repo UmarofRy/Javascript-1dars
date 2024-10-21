82f4e2a7384d174e4ee35df02c51847ccdea5fcf
RyUmarof <ryumarof@gmail.com>
2024-10-21 16:25:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//let
+//var
+//const
+
+let ism = prompt('ismingizni kiriting')
+let yosh = prompt('familyangizni kiriting')
+let kun = prompt('yoshingizni kiriting')
+let a = prompt('yashaydigan tumaningizni kiriting')
+let nmani = prompt('yoqtirgan faningiz')
+alert('Malumotlar uchun raxmat!' )
+
+console.log(ism);
+console.log(yosh);
+console.log(kun);
+console.log(a);
+console.log(nmani);
